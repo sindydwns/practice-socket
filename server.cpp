802b28f0693096a3684f3f96bbdbec5cf4efdc59
sindydwns <sindydwns@naver.com>
2023-09-21 16:46:55 +0900

    fix

--- server.cpp
+++ server.cpp
@@ -7,6 +7,11 @@
 #include <algorithm>
 #include <string.h>
 
+// for kqueue
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+
 #define SIZE 42
 
 int main() {
@@ -24,46 +29,40 @@ int main() {
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, 3);
 
-	fd_set read_fds;
-	int max_fd = server_fd;
+	int kq = kqueue();
+	struct kevent serverEvent;
+	EV_SET(&serverEvent, server_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+	kevent(kq, &serverEvent, 1, NULL, 0, NULL);
 
 	while (1) {
-		FD_ZERO(&read_fds);
-		FD_SET(server_fd, &read_fds);
-		for (size_t i = 0; i < clientSockets.size(); i++) {
-			int sock = clientSockets[i];
-			FD_SET(sock, &read_fds);
-		}
+		struct kevent eventList[1024];
+		int nev = kevent(kq, NULL, 0, eventList, 1024, NULL);
+		for (int i = 0; i < nev; i++) {
+			int fd = eventList[i].ident;
 
-
-		int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
-		if (activity < 0) {
-			std::cout << "error" << std::endl;
-		}
-
-		if (FD_ISSET(server_fd, &read_fds)) {
-			int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-			if (new_socket > max_fd) {
-				max_fd = new_socket;
+			if (fd == server_fd) {
+				int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+				clientSockets.push_back(new_socket);
+				std::cout << "new_socket [" << new_socket << "]" << std::endl;
+				
+				struct kevent clientEvent;
+				EV_SET(&clientEvent, new_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
+				kevent(kq, &clientEvent, 1, NULL, 0, NULL);
 			}
-			clientSockets.push_back(new_socket);
-		}
-
-		for (size_t i = 0; i <= clientSockets.size(); i++) {
-			int sock = clientSockets[i];
-			if (FD_ISSET(sock, &read_fds)) {
+			else {
 				memset(buffer, 0, SIZE);
-				int valread = read(sock, buffer, SIZE - 1);
-				std::cout << sock << ": " << buffer << "[" << valread << "]" << std::endl;
+				int valread = read(fd, buffer, SIZE - 1);
+				std::cout << fd << ": " << buffer << "[" << valread << "]" << std::endl;
 				if (valread == 0) {
-					clientSockets.erase(std::find(clientSockets.begin(), clientSockets.end(), sock));
-					std::cout << sock << " closed" << std::endl;
-					close(sock);
+					clientSockets.erase(std::find(clientSockets.begin(), clientSockets.end(), fd));
+					std::cout << fd << " closed" << std::endl;
+					close(fd);
 				}
-				send(sock, buffer, valread, 0);
+				else {
+					send(fd, buffer, valread, 0);
+				}
 			}
 		}
-
 	}
     return 0;
 }
